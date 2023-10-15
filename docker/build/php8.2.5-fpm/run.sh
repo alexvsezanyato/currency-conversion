184043ed53c9c34c346fe4779f59d4a031b03e10
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2023-10-16 04:44:37 +0700

    Fix `permission denied` (add manager to www-data groups

--- docker/build/php8.2.5-fpm/run.sh
+++ docker/build/php8.2.5-fpm/run.sh
@@ -1,6 +1,7 @@
 groupadd --gid ${UID} manager
 useradd --uid ${UID} --gid ${GID} --groups www-data --shell /bin/bash manager --create-home
+usermod --groups ${UID} www-data
 
-echo 'alias a="php /laravel/artisan"' >> /home/manager/.bashrc
-echo 'alias artisan="php /laravel/artisan"' >> /home/manager/.bashrc
+echo 'alias a="php /site/artisan"' >> /home/manager/.bashrc
+echo 'alias artisan="php /site/artisan"' >> /home/manager/.bashrc
 echo 'alias c="composer"' >> /home/manager/.bashrc
