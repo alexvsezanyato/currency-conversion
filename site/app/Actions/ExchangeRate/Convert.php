1eb42b66daf8f1c7737ec765444b09bcdbe29055
alexvsezanyato <stepin.aleksandr.dj@gmail.com>
2023-10-18 22:59:43 +0700

    Add ExchangeRate\Convert action

---
+++ site/app/Actions/ExchangeRate/Convert.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions\ExchangeRate;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class Convert
+{
+    use AsAction;
+
+    public function handle($amount, $from, $to)
+    {
+    }
+}
